b74b4bbe65ba7c86be6dbcea5684ae4ee6aed5be
ebel3000 <bellomoemi@gmail.com>
2024-09-26 17:25:35 -0300

    add footer and gif

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 <?php require "partials/header.php" ?>
 
 
+<main>
+
+    <div class="row">
+
+        <div>
+<!-- card -->
 
 
 
-<?php  require "partials/footer.php"  ?>
+        <div class="d-flex justify-content-center">
+<!-- foto -->
+ <img src="img/hansolo.gif" alt="">
+
+
+            </div>
+        </div>
+
+
+    </div>
+
+</main>
+
+
+
+
+<?php require "partials/footer.php"  ?>
--- partials/footer.php
+++ partials/footer.php
@@ -1,15 +1,13 @@
+<footer>
+    <div class= "p-2">
+        <div  class="d-flex justify-content-between ">
+            <h2 class="text-white">Todos los derechos reservados</h2>
+            <img width="50px" src="img/R2D2_footer.png" alt="">
+        </div>
+    </div>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
-
-
-<div>
-    <div>
-        <h2>Todos los derechos reservados</h2>
-        <img src="" alt="">
-    </div>
-
-
-
-</div>
