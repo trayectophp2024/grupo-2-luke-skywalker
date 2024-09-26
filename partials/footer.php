2601db85d7af15699d6efa4cc713d1461f35f441
ebel3000 <bellomoemi@gmail.com>
2024-09-26 17:05:18 -0300

    added header

---
+++ partials/footer.php
@@ -0,0 +1,15 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+
+<div>
+    <div>
+        <h2>Todos los derechos reservados</h2>
+        <img src="" alt="">
+    </div>
+
+
+
+</div>
---
+++ partials/header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/estilos.css">
+  </head>
+  <body>
+
+   <header>
+
+      <div class="header-superior container">
+              <div class="row mt-3 d-flex justify-content-around ">
+                  <div class="col-2">
+                        <img width="200px" src="img/estrella_header.png" alt="">
+                  </div>
+                  <div class="col-8">
+                      <h1 class="text-white">EL UNIVERSO DE STAR WARS</h1>
+                      <form action="" method="post">
+                          <input class="form-control" type="search" name="" id="">
+                      </form>
+                  </div>
+              </div>
+      </div>
+
+      <div class="header-inferior">
+
+      </div>
+
+   </header>
+    
